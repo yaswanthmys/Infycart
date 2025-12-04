d95abdab261366fffb458a86dc27979782e72ac9
MYSation <yaswanthmadineni@gmail.com>
2025-12-04 12:37:29 +0530

    made changes to css and html files

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -11,3 +11,24 @@ document.getElementById("product-share").addEventListener("click", () => {
         alert("Sharing not supported on this device")
     }
 });
+
+const track = document.querySelector(".product-imgHolder");
+const slides = document.querySelectorAll(".product-displayImg");
+const prev = document.querySelector(".product-leftBtn");
+const next = document.querySelector(".product-rightBtn");
+
+let index = 0;
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+next.onclick = () => {
+    index = (index + 1) % slides.length; // infinite loop
+    updateCarousel();
+};
+
+prev.onclick = () => {
+    index = (index - 1 + slides.length) % slides.length; // infinite loop
+    updateCarousel();
+};
